e522f40c0aec0832b88d3bc6949a8df29391cdfa
yuhei.kasai <yuhei@Air.local>
2020-06-26 02:09:17 +0900

    edit delete の作成

--- app/controllers/debates_controller.rb
+++ app/controllers/debates_controller.rb
@@ -21,8 +21,27 @@ class DebatesController < ApplicationController
     end
   end
 
+  def edit
+    @debate = Debate.find(params[:id])
+  end
+
+  def update
+    @debate = Debate.find(params[:id])
+    if @debate.update(debate_params)
+      redirect_to debates_path
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @debate = Debate.find(params[:id])
+    @debate.destroy
+    redirect_to debates_path
+  end
+
   private
     def debate_params
-      params.require(:post).permit(:title, :body)
+      params.require(:debate).permit(:title, :body)
     end
 end
